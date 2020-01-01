4f93ff1c0b0fe1ce0079702a2679df23fc5a7c1f
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-31 21:55:44 -0500

    Creates mongoDB connection.

--- server/server.js
+++ server/server.js
@@ -4,3 +4,15 @@ let mongoose = require('mongoose');
 let cors = require('cors');
 let bodyParser = require('body-parser');
 let dbConfig = require('./database/db');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+  useNewUrlParser: true
+})
+.then(() => {
+  console.log('Database successfully connected');
+},
+  error => {
+    console.log('Database could not connect: ' + error)
+  }
+);
