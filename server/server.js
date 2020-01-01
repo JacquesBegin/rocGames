4ddce92a7efa2daef9e6c35e248e2fc2c7ba34b4
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-31 22:56:02 -0500

    Loads routes for profiles and articles.

--- server/server.js
+++ server/server.js
@@ -4,3 +4,5 @@ let cors = require('cors');
 let bodyParser = require('body-parser');
 let db = require('db/db.connection');
 
+const profileRoute = require('./routes/profile');
+const articleRoute = require('./routes/article');
