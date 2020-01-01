98eb15542035cb8bf2674993edfe8b0b3dd6cb69
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-31 21:59:54 -0500

    Moves db connection to separate file.

--- server/server.js
+++ server/server.js
@@ -1,18 +1,6 @@
 let express = require('express');
 let path = require('path');
-let mongoose = require('mongoose');
 let cors = require('cors');
 let bodyParser = require('body-parser');
 let dbConfig = require('./database/db');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-  useNewUrlParser: true
-})
-.then(() => {
-  console.log('Database successfully connected');
-},
-  error => {
-    console.log('Database could not connect: ' + error)
-  }
-);
