7fb6c59a09d4807dbba54d7d39503539047aa619
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-31 21:51:04 -0500

    Adds additional require statements.

--- server/server.js
+++ server/server.js
@@ -1,2 +1,6 @@
 let express = require('express');
 let path = require('path');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let dbConfig = require('./database/db');
