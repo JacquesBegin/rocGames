75b1813999b4f119bf809f5042e45a1c907bccb3
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-31 23:00:01 -0500

    Creates an instance of express.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ let express = require('express');
 let path = require('path');
 let cors = require('cors');
 let bodyParser = require('body-parser');
+
 let db = require('db/db.connection');
 
 const profileRoute = require('./routes/profile');
 const articleRoute = require('./routes/article');
+
+const app = express();
