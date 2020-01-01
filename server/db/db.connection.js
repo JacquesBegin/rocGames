5566dcdaf8d3d1263d2b8469a1abd83a1012d1bc
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-31 22:47:11 -0500

    Exports db connection function.

--- server/db/db.connection.js
+++ server/db/db.connection.js
@@ -1,13 +1,20 @@
 let mongoose = require('mongoose');
+let dbConfig = require('./database/db/dbConfig');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-  useNewUrlParser: true
-})
-.then(() => {
-  console.log('Database successfully connected');
-},
-  error => {
-    console.log('Database could not connect: ' + error)
-  }
-);
+function connectToDB() {
+  mongoose.Promise = global.Promise;
+  mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log('Database successfully connected');
+  },
+    error => {
+      console.log('Database could not connect: ' + error)
+    }
+  );
+}
+
+module.exports = {
+  connect: connectToDB()
+}
