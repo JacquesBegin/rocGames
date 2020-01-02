f5d4466940529a017946d2c8687c3521730db6a4
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-01 22:29:42 -0500

    Add delete article route.

--- server/routes/article/article.route.js
+++ server/routes/article/article.route.js
@@ -50,3 +50,18 @@ articleRoute.route('/update/:id').put((req, res, next) => {
     }
   });
 });
+
+// Delete article
+articleRoute.route('/delete/:id').delete((req, res, next) => {
+  Article.findOneAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data
+      });
+    }
+  });
+});
+
+module.exports = articleRoute;
