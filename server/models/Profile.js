f8acfcaedf296ff82c1c1f3e495e77f68933bf60
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-01 21:55:38 -0500

    Creates Profile schema.

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,3 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+let Profile = new Schema({
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  userName: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  }
+}, {
+  collection: 'profile'
+});
+
+module.exports = mongoose.model('Profile', Profile);
