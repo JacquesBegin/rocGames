c5ab36a374258843d29da8c77c0aeee06da36a1a
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-06 20:44:04 -0500

    Adds more player data.

--- roc-games/src/app/components/theTeam/the-team/the-team.component.ts
+++ roc-games/src/app/components/theTeam/the-team/the-team.component.ts
@@ -9,16 +9,16 @@ export class TheTeamComponent implements OnInit {
 
   players: [
     {
-      playerName: "Bob 'Guns-a-Blazin' Jones",
-      playerPicture: "",
+      playerName: "Bob \"Guns-a-Blazin\" Jones",
+      playerPicture: "../../../../assets/images/temp/blazin.jpg",
       playerAchievements: ["Holds the East Coast record for rat tails collected with one wing tied behind his back."],
-      playerBio: "'You see this hair baby.....that's all I've got to say'"
+      playerBio: "'You see this hair baby.....that\'s all I\'ve got to say'"
     },
     {
-      playerName: "",
+      playerName: "Mary \"Evil Eye\" Henderson",
       playerPicture: "",
-      playerAchievements: [],
-      playerBio: ""
+      playerAchievements: ["Once flew from New York to LA without flapping her wings."],
+      playerBio: "You lookin' at me chump?"
     },
     {
       playerName: "",
