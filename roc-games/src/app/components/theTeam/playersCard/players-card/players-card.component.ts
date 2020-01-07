2225d9f1334384416a3bb4128932022dfaeb09ba
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-07 17:07:05 -0500

    Adds temp player card data.

--- roc-games/src/app/components/theTeam/playersCard/players-card/players-card.component.ts
+++ roc-games/src/app/components/theTeam/playersCard/players-card/players-card.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersCardComponent implements OnInit {
 
+  players: [
+    {
+      playerName: "Bob \"Guns-a-Blazin\" Jones",
+      playerPicture: "../../../../assets/images/temp/blazin.jpg",
+      playerAchievements: ["Holds the East Coast record for rat tails collected with one wing tied behind his back."],
+      playerBio: "'You see this hair baby.....that\'s all I\'ve got to say'"
+    },
+    {
+      playerName: "Mary \"Evil Eye\" Henderson",
+      playerPicture: "../../../../assets/images/temp/kingUrubu.jpg",
+      playerAchievements: ["Once flew from New York to LA without flapping her wings."],
+      playerBio: "You lookin\' at me chump?"
+    },
+    {
+      playerName: "Tommy \"Chicken Scratch\" Wilburn",
+      playerPicture: "../../../../assets/images/temp/chickenScratch.jpg",
+      playerAchievements: ["Pecked his way through the farmers market without becoming dinner."],
+      playerBio: "I can scratch with the best of \'em"
+    },
+    {
+      playerName: "Sally \"Two Cans\" Miller",
+      playerPicture: "../../../../assets/images/temp/toucanSally.jpg",
+      playerAchievements: ["Can open any can without breaking a sweat."],
+      playerBio: "You'll be lucky if I show up"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
