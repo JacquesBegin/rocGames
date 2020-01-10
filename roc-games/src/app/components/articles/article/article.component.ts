f52d94f6fea306da505cf1a70db7d09be8c8d5eb
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-09 20:59:59 -0500

    Adds temp content to article.

--- roc-games/src/app/components/articles/article/article.component.ts
+++ roc-games/src/app/components/articles/article/article.component.ts
@@ -12,7 +12,7 @@ export class ArticleComponent implements OnInit {
       articleTitle: "The Best Way to Win at Losing",
       articleAuthor: "Guns-a-Blazin",
       articleDate: "",
-      articleContent: ""
+      articleContent: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit turpis eleifend mollis volutpat. Fusce mollis diam orci, non sagittis felis pharetra sit amet. Integer id nunc sem. Mauris eu quam molestie, congue ex quis, viverra purus. Integer ornare justo et aliquam fringilla. Maecenas ut sodales lorem, quis vehicula ante. Morbi vitae elit urna. Phasellus nisi tellus, commodo eget consequat at, dictum tristique urna. Ut porttitor dapibus magna, sit amet faucibus dolor cursus et. Quisque pellentesque lorem dolor, non dictum ligula volutpat a. <br><br> Donec tincidunt eleifend ipsum, eu ornare diam ullamcorper at. Nulla ac tellus neque. Cras sapien velit, rutrum at accumsan at, pellentesque sed diam. Sed interdum, purus congue ultricies consequat, lorem elit sollicitudin diam, non vulputate arcu sapien in nibh. Morbi vulputate, tortor ac fringilla elementum, lacus nibh convallis justo, ac aliquet leo quam ut nisl. Maecenas ornare purus interdum, convallis tortor vel, dapibus risus. Suspendisse auctor diam mattis molestie scelerisque. Suspendisse quis mattis elit.</p>"
     },
     {
       articleTitle: "",
