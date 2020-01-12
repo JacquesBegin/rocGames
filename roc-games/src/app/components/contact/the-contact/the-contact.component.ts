ee0d9aabfcf8f7efc6510a05461dad42dd884a81
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-11 21:13:19 -0500

    Adds string variable for contact name.

--- roc-games/src/app/components/contact/the-contact/the-contact.component.ts
+++ roc-games/src/app/components/contact/the-contact/the-contact.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TheContactComponent implements OnInit {
 
+  contactName: string = "";
+  
+
   constructor() { }
 
   ngOnInit() {
