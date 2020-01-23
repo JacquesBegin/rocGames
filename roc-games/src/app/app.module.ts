18eecd5097dde90705840bfbd5eb79c0ad1b3224
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-22 23:28:21 -0500

    Adds httpclient to app module.

--- roc-games/src/app/app.module.ts
+++ roc-games/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
@@ -35,7 +36,8 @@ import { ContactFormComponent } from './components/contact/contact-form/contact-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
